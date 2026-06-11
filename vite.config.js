713106dac7a962cfdfda5a74aff8cd0ace344f1d
Pedro <pedrofbks@gmail.com>
2026-06-11 18:13:26 -0300

    chore: atualiza base para github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vue-prototype/',
   plugins: [vue()],
 })
 // Restarting server to pick up new npm dependencies
